e3cb4870d014a65d377a19bcaa65af24a68ffd84
JohnRicMerque <johnricmer@gmail.com>
2022-09-27 06:28:14 +0800

    applied styles based on the reference

--- fm-qr-code-component/tailwind.config.js
+++ fm-qr-code-component/tailwind.config.js
@@ -2,13 +2,20 @@
 module.exports = {
   content: ["./qr-code-component-main/index.html"],
   theme: {
-    colors: {
+    extend: {
+      colors: {
       white: '#ffffff',
       light_gray: '#d6e2f0',
       grayish_blue: '#7b879d',
       dark_blue: '#1f3251'
     },
-    extend: {},
+      fontFamily: {
+        outfit: ['Outfit']
+    }, 
+      fontSize: {
+        primary: '15px'
+    }
+    }
   },
   plugins: [],
 }
