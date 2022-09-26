5ecf7a81cca317117a0021ca1cc3d83af431c2dd
JohnRicMerque <johnricmer@gmail.com>
2022-09-26 21:01:00 +0800

    added and centered qr code

--- fm-qr-code-component/tailwind.config.js
+++ fm-qr-code-component/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./qr-code-component-main/index.html"],
   theme: {
     colors: {
+      white: '#ffffff',
       light_gray: '#d6e2f0',
       grayish_blue: '#7b879d',
       dark_blue: '#1f3251'
