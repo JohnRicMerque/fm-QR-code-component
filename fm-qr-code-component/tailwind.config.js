1606bdeb48979b3defaf4bea7da55fa4797fffd8
JohnRicMerque <johnricmer@gmail.com>
2022-09-26 20:37:33 +0800

    initialized the materials and tailwindcss

---
+++ fm-qr-code-component/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./qr-code-component-main/index.html"],
+  theme: {
+    colors: {
+      light_gray: '#d6e2f0',
+      grayish_blue: '#7b879d',
+      dark_blue: '#1f3251'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
