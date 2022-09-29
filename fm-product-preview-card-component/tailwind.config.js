27066131d1ef1efd91e723fcaf3d1afda8be52ed
John Ric Merque <johnricmer@gmail.com>
2022-09-29 08:28:45 +0800

    added photo, background, text and fonts

--- fm-product-preview-card-component/tailwind.config.js
+++ fm-product-preview-card-component/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        cream: "#f2ebe3"
+        cream: "#f2ebe3",
+        dark_cyan: "hsl(158, 36%, 37%)",
+        very_dark_blue: "hsl(212, 21%, 14%)",
+        dark_grayish_blue: "hsl(228, 12%, 48%)",
+      }, 
+      fontFamily: {
+        monteserrat: ['Montserrat'],
+        fraunces: ['Fraunces']
       }
     },
   },
