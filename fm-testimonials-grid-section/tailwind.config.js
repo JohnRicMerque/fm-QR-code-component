0ddadd516bc9b657c3059b4d948b9567090fc002
John Ric Merque <johnricmer@gmail.com>
2022-10-05 11:32:18 +0800

    added necessary colors and fonts to the config

--- fm-testimonials-grid-section/tailwind.config.js
+++ fm-testimonials-grid-section/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Moderate_violet: "hsl(263, 55%, 52%)",
+        Very_dark_grayish_blue: "hsl(217, 19%, 35%)",
+        Very_dark_blackish_blue: "hsl(219, 29%, 14%)",
+        White: "hsl(0, 0%, 100%)",
+        Light_gray: "hsl(0, 0%, 81%)",
+        Light_grayish_blue: "hsl(210, 46%, 95%)"
+      }, 
+      fontFamily: {
+        barlow_semi_condensed: ['Barlow Semi Condensed']
+      }
+    },
   },
   plugins: [],
 }
